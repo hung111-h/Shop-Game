d02be8698573030d5d37b0440c6e2c5c1e5d743a
hung111-h <16142124@student.hcmute.edu.vn>
2021-03-20 21:18:07 +0700

    summit-10

--- du-an-cuoi-khoa.js
+++ du-an-cuoi-khoa.js
@@ -218,4 +218,13 @@ document.getElementById("name-1").addEventListener('keydown',function (event) {
     }
   })
 
-
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'phamminhhung609@gmail.com',
+    from: 'test@example.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
